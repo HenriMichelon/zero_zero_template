98634402f6ab51afccca597eb4cddc9f7ba68610
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-06-20 11:57:56 +0200

    More advanced template

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ void MainScene::onEnterScene() {
     auto buttonQuit = make_shared<GButton>();
     menu->getWidget().add(buttonQuit, GWidget::LEFTCENTER, "50,40");
     buttonQuit->add(textQuit, GWidget::CENTER);
-    buttonQuit->connect(GEvent::OnClick, this, GEventFunction(&MainScene::onMenuQuit));
+    buttonQuit->connect(GEvent::OnClick, this, Signal::Handler(&MainScene::onMenuQuit));
 
     menu->setHeight(textQuit->getHeight() + padding * 4);
     menu->setWidth(textQuit->getWidth() + padding * 4);
@@ -23,10 +23,96 @@ void MainScene::onEnterScene() {
 
 void MainScene::onReady() {
     log("Example ready");
+
+    addChild(make_shared<Skybox>("res/textures/StandardCubeMap.png"));
+    addChild(make_shared<Environment>(vec4{1.0,1.0,1.0,0.6f}));
+    auto directionalLight = make_shared<DirectionalLight>(
+        vec3{-1.0f, -1.0f, -1.0f},
+        vec4{-0.5f, -0.5f, 1.0f, 1.0f}
+    );
+    directionalLight->setCastShadow(true);
+    addChild(directionalLight);
+
+    player = make_shared<Node>();
+    player->setPosition({0.0f, 0.0f, 0.5f});
+    addChild(player);
+
+    cameraPivot = make_shared<Node>();
+    player->addChild(cameraPivot);
+
+    auto camera = make_shared<Camera>();
+    cameraPivot->addChild(camera);
+
+    cube = Loader::loadModelFromFile("res/models/cube.glb");
+    addChild(cube);
+
+    captureMouse();
 }
 
-void MainScene::onMenuQuit(GWidget*, GEvent *) {
+void MainScene::onPhysicsProcess(float delta) {
+    previousState = currentState;
+    currentState = State{};
+
+    if (mouseCaptured) {
+        vec2 inputDir = Input::getKeyboardVector(KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN);
+        if (inputDir != VEC2ZERO) {
+            currentState.lookDir = inputDir * viewSensitivity * delta;
+        }
+    }
+
+    auto angle = delta * radians(90.0f) / 2;
+    cube->rotateX(angle);
+    cube->rotateZ(angle);
+}
+
+void MainScene::onProcess(float alpha) {
+    if (currentState.lookDir != VEC2ZERO) {
+        auto interpolatedLookDir = previousState.lookDir * (1.0f-alpha) + currentState.lookDir * alpha;
+        player->rotateY(-interpolatedLookDir.x * 2.0f);
+        cameraPivot->rotateX(interpolatedLookDir.y * keyboardInvertedAxisY);
+        cameraPivot->setRotationX(std::clamp(cameraPivot->getRotationX() , maxCameraAngleDown, maxCameraAngleUp));
+    }
+}
+
+bool MainScene::onInput(InputEvent& event) {
+    if (mouseCaptured && (event.getType() == INPUT_EVENT_MOUSE_MOTION)) {
+        auto& eventMouseMotion = dynamic_cast<InputEventMouseMotion&>(event);
+        player->rotateY(-eventMouseMotion.getRelativeX() * mouseSensitivity);
+        cameraPivot->rotateX(eventMouseMotion.getRelativeY() * mouseSensitivity * mouseInvertedAxisY);
+        cameraPivot->setRotationX(std::clamp(cameraPivot->getRotationX(), maxCameraAngleDown, maxCameraAngleUp));
+        return true;
+    }
+    if ((event.getType() == INPUT_EVENT_MOUSE_BUTTON) && (!mouseCaptured)) {
+        auto& eventMouseButton = dynamic_cast<InputEventMouseButton&>(event);
+        if (!eventMouseButton.isPressed()) {
+            captureMouse();
+            return true;
+        }
+    }
+    if ((event.getType() == INPUT_EVENT_KEY) && mouseCaptured) {
+        auto& eventKey = dynamic_cast<InputEventKey&>(event);
+        if ((eventKey.getKey() == KEY_ESCAPE) && !eventKey.isPressed()) {
+            releaseMouse();
+            return true;
+        }
+    }
+    return false;
+}
+
+void MainScene::onMenuQuit() {
     app().quit();
+}
+
+void MainScene::captureMouse() {
+    if (!mouseCaptured) {
+        Input::setMouseMode(MOUSE_MODE_HIDDEN_CAPTURED);
+        mouseCaptured = true;
+    }
+}
+
+void MainScene::releaseMouse() {
+    Input::setMouseMode(MOUSE_MODE_VISIBLE);
+    mouseCaptured = false;
 }
 
 const ApplicationConfig applicationConfig {
@@ -35,8 +121,8 @@ const ApplicationConfig applicationConfig {
     .windowMode = WINDOW_MODE_FULLSCREEN,
     .windowWidth = 1920,
     .windowHeight = 1080,
-    .defaultFontName = "res/fonts/OpenSans-Regular.ttf",
-    .defaultFontSize = 12,
+    .defaultFontName = "res/fonts/MontserratMedium.otf",
+    .defaultFontSize = 30,
     .loggingMode = static_cast<LoggingMode>(LOGGING_FILE | LOGGING_WINDOW)
 };
 
