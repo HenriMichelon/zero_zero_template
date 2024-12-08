30023ba893f53d9e3c31b53f5a479e87e3b7b33c
Henri <148531229+HenriMichelon@users.noreply.github.com>
2024-12-08 22:41:48 +0100

    Manually selecting submit type

--- src/libraries.h
+++ src/libraries.h
@@ -1,10 +1,19 @@
+/*
+ * Copyright (c) 2024 Henri Michelon
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+*/
 #pragma once
 
+#ifdef _MSC_VER
 #include <glm/glm.hpp>
-using namespace glm;
-
+#else
+import glm;
+#endif
 import std;
-using namespace std;
-
 import z0;
+
+using namespace glm;
+using namespace std;
 using namespace z0;
